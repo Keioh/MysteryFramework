93593dc6fe381a43ba2c3563ebccf82d05b12338
KouKeioh <kou2013kou.kou@gmail.com>
2025-07-09 10:06:10 +0900

    コメントの追加

--- MF/Module/FileIO.py
+++ MF/Module/FileIO.py
@@ -5,22 +5,28 @@ class EasyFileIO:
     #全行をリストとして読み込む
     def EasyOpenRead(self, filePath):
         
+        #取得した全てのテキストデータ
         textLineData = []
+
+        #取得した一行のテキストデータ
         line = []
 
+        #ファイルの読み込み
         file = open(filePath, encoding="utf-8")
 
+        #ファイル末端まで繰り返す
         while line != '':
 
             line = file.readline()
 
+            #リストにデータを追加
             textLineData.append(line)
 
         file.close()
 
         return textLineData
     
-    
+
     #リストに纏めた文字列をファイルに書き込む
     def EasyOpenWrite(self, filePath, lineList:str):
 
