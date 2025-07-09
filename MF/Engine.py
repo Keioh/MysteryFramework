bc938a77f7ea2cf7d4f9ae6ecc590a52e837bfe8
KouKeioh <kou2013kou.kou@gmail.com>
2025-07-09 11:10:22 +0900

    ループ処理の修正 　最初のループ開始時に二回Updateを行ってしまう問題の修正

--- MF/Engine.py
+++ MF/Engine.py
@@ -1,11 +1,15 @@
 """実行環境"""
 import App
 
+#フレームワークの作成
 MF = App.MysteryFramework()
+
+#ループコードの保存変数
+LoopCode = 0
 
 #一度だけの処理
 MF.Start()
 
 #ループ処理
-while MF.Update() == App.Mystery.LoopErr.Code.Looping:
-    MF.Update()
+while LoopCode == App.Mystery.LoopErr.Code.Looping:
+    LoopCode = MF.Update()
